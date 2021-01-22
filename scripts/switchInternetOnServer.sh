a0ae29275a31d4226369dd3216cb7f91e5d6a163
root <dev.cour.de.cassation@gmail.com>
2021-01-22 16:32:22 +0100

    add no proxy for localhost

--- scripts/switchInternetOnServer.sh
+++ scripts/switchInternetOnServer.sh
@@ -11,10 +11,12 @@ else
   then
     export http_proxy='http://rie-proxy.justice.gouv.fr:8080/'
     export https_proxy='http://rie-proxy.justice.gouv.fr:8080/'
+    export no_proxy='localhost,127.0.0.0,127.0.0.1,127.0.1.1'
   elif [ $1 = "off"  ]
   then
     unset http_proxy
     unset https_proxy    
+    unset no_proxy
   else
     echo $USAGE
   fi

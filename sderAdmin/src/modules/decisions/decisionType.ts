e66b50a5d8984da7368fed2b221e8c440152678f
Nicolas Assouad <nicolas@assouad.com>
2021-02-09 14:50:26 +0100

    new decision type

---
+++ sderAdmin/src/modules/decisions/decisionType.ts
@@ -0,0 +1,35 @@
+import { mongoIdType } from '../../utils';
+
+export type { decisionType };
+
+type decisionType = {
+  _id: mongoIdType;
+  _rev: number;
+  analysis: {
+    doctrine: string;
+    link: string;
+    reference: Array<string>;
+    source: string;
+    summary: string;
+    target: string;
+    title: Array<string>;
+  };
+  appeals: Array<string>;
+  chamberId: string;
+  chamberName: string;
+  dateCreation: Date;
+  dateDecision: Date;
+  jurisdictionCode: string;
+  jurisdictionId: string;
+  jurisdictionName: string;
+  locked: string;
+  originalText: string;
+  parties: string;
+  pseudoStatus: string;
+  pseudoText: string;
+  pubCategory: string;
+  registerNumber: string;
+  solution: string;
+  sourceId: string;
+  sourceName: string;
+};

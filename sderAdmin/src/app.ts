d566401190f42d6f503cfcf8ed9791b99494a0fa
Nicolas Assouad <nicolas@assouad.com>
2021-02-12 12:05:49 +0100

    route and controller

---
+++ sderAdmin/src/app.ts
@@ -0,0 +1,29 @@
+import bodyParser from 'body-parser';
+import express = require('express');
+
+import { environment } from './environment';
+import { decisionService } from './modules';
+import { buildHandlingErrorController } from './utils';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get(
+  '/label/decisions-to-pseudonymise',
+  buildHandlingErrorController(async (req: any) =>
+    decisionService.fetchDecisionsToPseudonymise({ date: new Date(JSON.parse(req.query.date)) }),
+  ),
+);
+
+app.patch(
+  '/label/decisions-to-pseudonymise',
+  buildHandlingErrorController(async (req: any) => decisionService.setDecisionLoadedInLabel(req.body)),
+);
+
+app.patch(
+  '/label/update-decision-pseudonymisation',
+  buildHandlingErrorController(async (req: any) => decisionService.updateDecisionPseudonymisation(req.body)),
+);
+
+app.listen(environment.port);
--- sderAdmin/src/environment.ts
+++ sderAdmin/src/environment.ts
@@ -1,6 +1,7 @@
 export { environment };
 
 const environment = {
+  port: 54321,
   SDER_DB_URL: 'mongodb://bkpanonym:27017',
   SDER_DB_NAME: 'SDER',
 };

c6d32523aad09c5208c3642e8acc8b599de95073
Nicolas Assouad <nicolas@assouad.com>
2021-02-12 11:07:19 +0100

    decision repository

---
+++ sderAdmin/src/modules/decisions/repository/buildDecisionFakeRepository.ts
@@ -0,0 +1,57 @@
+import { areMongoIdEqual } from '../../../utils';
+import { decisionType } from '../decisionType';
+import { decisionRepositoryType } from './decisionRepositoryType';
+
+export { buildDecisionFakeRepository };
+
+const collectionName = 'decisions';
+let collection = [] as decisionType[];
+
+async function buildDecisionFakeRepository(): Promise<decisionRepositoryType> {
+  return {
+    async clear() {
+      collection = [];
+    },
+
+    async findAll() {
+      return collection;
+    },
+
+    async findAllToPseudonymiseSince(date) {
+      return collection.filter(
+        (decision) =>
+          decision.dateCreation >= date && (decision.isLoadedInLabel === false || decision.pseudoText === ''),
+      );
+    },
+
+    async findById(id) {
+      const result = collection.find((decision) => areMongoIdEqual(decision._id, id));
+
+      if (!result) {
+        throw new Error(`No matching ${collectionName} for _id ${id}`);
+      }
+
+      return result;
+    },
+
+    async findByDecisionId(decisionId) {
+      const result = collection.find((decision) => decision.sourceId === decisionId);
+
+      if (!result) {
+        throw new Error(`No matching ${collectionName} for sourceId ${decisionId}`);
+      }
+
+      return result;
+    },
+
+    async insert(decision) {
+      collection.push(decision);
+    },
+
+    async updateById(id, decisionField) {
+      collection = collection.map((decision) =>
+        areMongoIdEqual(decision._id, id) ? { ...decision, ...decisionField } : decision,
+      );
+    },
+  };
+}

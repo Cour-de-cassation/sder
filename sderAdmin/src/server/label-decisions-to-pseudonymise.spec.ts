a43ef1ea33b3e92272ee2f52bab57bdb6f56ee40
Nicolas Assouad <nicolas@assouad.com>
2021-02-16 15:41:47 +0100

    test all the routes

---
+++ sderAdmin/src/server/label-decisions-to-pseudonymise.spec.ts
@@ -0,0 +1,43 @@
+const supertest = require('supertest');
+
+import { decisionModule } from '../modules';
+import { dateBuilder } from '../utils';
+import { server } from './server';
+
+const request = supertest(server);
+
+describe('/label/decisions-to-pseudonymise', () => {
+  it('should return all the decision to pseudonymise since the given date', async () => {
+    const decisionRepository = await decisionModule.buildRepository();
+    const decisions = [
+      { dateCreation: dateBuilder.daysAgo(1) },
+      { dateCreation: dateBuilder.daysAgo(3) },
+      { dateCreation: dateBuilder.daysAgo(5) },
+    ].map(decisionModule.lib.generateDecision);
+    await Promise.all(decisions.map(decisionRepository.insert));
+
+    const response = await request.get(
+      `/label/decisions-to-pseudonymise?date="${dateBuilder.daysAgo(4).toISOString()}"`,
+    );
+
+    expect(response.status).toEqual(200);
+    expect(JSON.stringify(response.body.sort(), null, 2)).toEqual(
+      JSON.stringify([decisions[0], decisions[1]].sort(), null, 2),
+    );
+  });
+
+  it('should return an empty array if there are no decision to pseudonymise since the given date', async () => {
+    const decisionRepository = await decisionModule.buildRepository();
+    const decisions = [{ dateCreation: dateBuilder.daysAgo(3) }, { dateCreation: dateBuilder.daysAgo(5) }].map(
+      decisionModule.lib.generateDecision,
+    );
+    await Promise.all(decisions.map(decisionRepository.insert));
+
+    const response = await request.get(
+      `/label/decisions-to-pseudonymise?date="${dateBuilder.daysAgo(2).toISOString()}"`,
+    );
+
+    expect(response.status).toEqual(200);
+    expect(JSON.stringify(response.body.sort(), null, 2)).toEqual(JSON.stringify([]));
+  });
+});
---
+++ sderAdmin/src/server/label-update-decision-pseudonymisation.spec.ts
@@ -0,0 +1,89 @@
+const supertest = require('supertest');
+
+import { decisionModule, decisionType } from '../modules';
+import { dateBuilder } from '../utils';
+import { server } from './server';
+
+const request = supertest(server);
+
+describe('/label/update-decision-pseudonymisation', () => {
+  it('should update the pseudonymisation text of the given decision', async () => {
+    const decisionRepository = await decisionModule.buildRepository();
+    const decisions = [{ pseudoText: '' }, { pseudoText: '' }].map(decisionModule.lib.generateDecision);
+    await Promise.all(decisions.map(decisionRepository.insert));
+
+    const response = await request.patch(`/label/update-decision-pseudonymisation`).send({
+      decisionId: decisions[0].sourceId,
+      decisionPseudonymisedText: 'NEW PSEUDO TEXT',
+      labelTreatments: [],
+    });
+
+    const updatedDecision = await decisionRepository.findByDecisionId(decisions[0].sourceId);
+    expect(response.status).toEqual(200);
+    expect(updatedDecision.pseudoText).toEqual('NEW PSEUDO TEXT');
+  });
+
+  it('should update the label treatments of the given decision', async () => {
+    const decisionRepository = await decisionModule.buildRepository();
+    const decisions = [{ labelTreatments: [] }, { labelTreatments: [] }].map(decisionModule.lib.generateDecision);
+    await Promise.all(decisions.map(decisionRepository.insert));
+    const labelTreatments = [
+      {
+        annotations: [
+          {
+            category: 'CATEGORY',
+            entityId: 'ENTITY_ID',
+            start: 0,
+            text: 'TEXT',
+          },
+        ],
+
+        source: 'SOURCE',
+        order: 0,
+      },
+    ];
+
+    const response = await request.patch(`/label/update-decision-pseudonymisation`).send({
+      decisionId: decisions[0].sourceId,
+      decisionPseudonymisedText: '',
+      labelTreatments,
+    });
+
+    const updatedDecision = await decisionRepository.findByDecisionId(decisions[0].sourceId);
+    expect(response.status).toEqual(200);
+    expect(updatedDecision.labelTreatments).toEqual(labelTreatments);
+  });
+
+  it('should not update the other decisions', async () => {
+    const decisionRepository = await decisionModule.buildRepository();
+    const decisions = [{ labelTreatments: [] }, { pseudoText: '', labelTreatments: [] }].map(
+      decisionModule.lib.generateDecision,
+    );
+    await Promise.all(decisions.map(decisionRepository.insert));
+
+    const response = await request.patch(`/label/update-decision-pseudonymisation`).send({
+      decisionId: decisions[0].sourceId,
+      decisionPseudonymisedText: 'NEW PSEUDO TEXT',
+      labelTreatments: [
+        {
+          annotations: [
+            {
+              category: 'CATEGORY',
+              entityId: 'ENTITY_ID',
+              start: 0,
+              text: 'TEXT',
+            },
+          ],
+
+          source: 'SOURCE',
+          order: 0,
+        },
+      ],
+    });
+
+    const decisionAfterUpdate = await decisionRepository.findByDecisionId(decisions[1].sourceId);
+    expect(response.status).toEqual(200);
+    expect(decisionAfterUpdate.labelTreatments).toEqual([]);
+    expect(decisionAfterUpdate.pseudoText).toEqual('');
+  });
+});

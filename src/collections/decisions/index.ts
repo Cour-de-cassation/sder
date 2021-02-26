1f5a8598ea6f87d39c8fe5959d66b463e36004a4
Nicolas ASSOUAD <nicolas@assouad.com>
2021-02-26 16:05:02 +0100

    set indexes (#16)

--- src/collections/decisions/index.ts
+++ src/collections/decisions/index.ts
@@ -1,9 +1,12 @@
+import { decisionType } from '../../modules';
 import { collectionType } from '../collectionType';
+import { decisionsIndexes } from './decisionsIndexes';
 import { decisionsValidationSchema } from './decisionsValidationSchema';
 
 export { decisionsCollection };
 
-const decisionsCollection: collectionType = {
+const decisionsCollection: collectionType<decisionType> = {
+  indexes: decisionsIndexes,
   name: 'decisions',
   validationSchema: decisionsValidationSchema,
 };

836cfbce2124b835409daacc28d0be528a01c484
Nicolas ASSOUAD <nicolas@assouad.com>
2021-03-03 11:27:44 +0100

    update mongo url (#20)

--- src/environment.ts
+++ src/environment.ts
@@ -2,6 +2,6 @@ export { environment };
 
 const environment = {
   port: 54321,
-  SDER_DB_URL: 'mongodb://bkpanonym:27017',
+  SDER_DB_URL: 'mongodb://127.0.0.1:27017',
   SDER_DB_NAME: 'SDER',
 };

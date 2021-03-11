bbe6b46fa662c5b132dde1bf4398e7bb49b245d6
Nicolas Assouad <nicolas@assouad.com>
2021-03-11 14:45:54 +0100

    correct status 413

--- src/server/server.ts
+++ src/server/server.ts
@@ -13,7 +13,7 @@ function startServer() {
   server.listen(environment.port);
 }
 
-server.use(bodyParser.json());
+server.use(bodyParser.json({ limit: '50mb' }));
 
 server.get(
   '/decisions-to-pseudonymise',

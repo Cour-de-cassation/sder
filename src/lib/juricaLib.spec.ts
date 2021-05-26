1ddc4b815437645666ced1331d5541b8a76721bc
BenoitSerrano <benoit.serrano10@gmail.com>
2021-05-26 15:41:17 +0200

    add test for juricaLib

---
+++ src/lib/juricaLib.spec.ts
@@ -0,0 +1,29 @@
+import { juricaLib } from './juricaLib';
+
+describe('juricaLib', () => {
+  describe('cleanText', () => {
+    it('should get rid of html tags', () => {
+      const text = '<em>italic</em>, <p>paragraph</ p> and <br /> new line.';
+
+      const cleanedText = juricaLib.cleanText(text);
+
+      expect(cleanedText).toBe('italic, paragraph and new line.');
+    });
+
+    it('should transform new lines and carriage returns', () => {
+      const text = 'Line 1\r\nLine 2\rLine 3';
+
+      const cleanedText = juricaLib.cleanText(text);
+
+      expect(cleanedText).toBe('Line 1\nLine 2\nLine 3');
+    });
+
+    it('should remove extra spaces', () => {
+      const text = 'Test with \\t\t\\f\fmultiple            spaces';
+
+      const cleanedText = juricaLib.cleanText(text);
+
+      expect(cleanedText).toBe('Test with multiple spaces');
+    });
+  });
+});

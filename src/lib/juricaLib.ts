5e63c40cd2cc69f24afaaf6b163645e859fd06fd
BenoitSerrano <benoit.serrano10@gmail.com>
2021-05-26 15:48:45 +0200

    extract juricaLib in subfunctions

--- src/lib/juricaLib.ts
+++ src/lib/juricaLib.ts
@@ -4,29 +4,41 @@ export { juricaLib };
 
 const juricaLib = {
   cleanText(text: string): string {
-    if (typeof text !== 'string') {
-      throw new Error('juricaLib.cleanText: text must be a string.');
-    }
+    assertIsTextString(text);
+    text = removeHTMLTags(text);
+    text = handleNewLines(text);
+    text = removeExtraSpaces(text);
 
-    if (text === '') {
-      throw new Error('juricaLib.cleanText: empty text, the document could be malformed or corrupted.');
-    }
-
-    // Remove all HTML tags:
-    text = text.replace(/<\/?[^>]+(>|$)/gm, '');
-
-    // Handling newlines and carriage returns:
-    text = text.replace(/\r\n/gim, '\n');
-    text = text.replace(/\r/gim, '\n');
-
-    // Remove extra spaces:
-    text = text.replace(/\t/gim, '');
-    text = text.replace(/\\t/gim, ''); // That could happen...
-    text = text.replace(/\f/gim, '');
-    text = text.replace(/\\f/gim, ''); // That could happen too...
-    text = text.replace(/  +/gm, ' ').trim();
-
-    // Decode HTML entities:
-    return decode(text);
+    return decode(text); // Decode HTML entities
   },
 };
+
+function assertIsTextString(text: string) {
+  if (typeof text !== 'string') {
+    throw new Error('juricaLib.cleanText: text must be a string.');
+  }
+
+  if (text === '') {
+    throw new Error('juricaLib.cleanText: empty text, the document could be malformed or corrupted.');
+  }
+}
+
+function removeExtraSpaces(text: string) {
+  text = text.replace(/\t/gim, '');
+  text = text.replace(/\\t/gim, '');
+  text = text.replace(/\f/gim, '');
+  text = text.replace(/\\f/gim, '');
+  text = text.replace(/  +/gm, ' ').trim();
+  return text;
+}
+
+function handleNewLines(text: string) {
+  text = text.replace(/\r\n/gim, '\n');
+  text = text.replace(/\r/gim, '\n');
+  return text;
+}
+
+function removeHTMLTags(text: string) {
+  text = text.replace(/<\/?[^>]+(>|$)/gm, '');
+  return text;
+}

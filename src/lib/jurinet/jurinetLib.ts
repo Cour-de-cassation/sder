09d18d32bb5d7b28ce2e0abcba42729716836ed3
Nicolas Assouad <nicolas@assouad.com>
2021-05-27 12:41:27 +0200

    extract and refacto the replacement of text arrets

--- src/lib/jurinet/jurinetLib.ts
+++ src/lib/jurinet/jurinetLib.ts
@@ -4,7 +4,6 @@ const jurinetLib = {
   cleanText(text: string): string {
     assertDecisionTextIsString(text);
 
-    // There could be more than one <TEXTE_ARRET> tags, so we first split the text around them:
     const texteArrets = extractTexteArrets(text);
 
     assertTexteArretsNotEmpty(texteArrets);
@@ -13,39 +12,7 @@ const jurinetLib = {
 
     assertCleanedTexteArretsNotEmpty(cleanedTexteArrets);
 
-    // Keep this info for later:
-    const textNextToCatPub = text.indexOf('</CAT_PUB><TEXTE_ARRET>') !== -1;
-
-    // Remove all <TEXT_ARRET> fragments from the text:
-    text = text.replace(/<texte_arret>[\s\S]*<\/texte_arret>/gim, '');
-
-    // Cleaning the rest of the text:
-    text = text
-      .replace(/&/g, '&amp;')
-      .replace(/&amp;amp;/g, '&amp;')
-      .replace(/&amp;#/g, '&#');
-    text = text.replace(/\s<\s/g, ' &lt; ');
-    text = text.replace(/\s>\s/g, ' &gt; ');
-
-    // A bad XML could lead to a bad JSON (the related data does not matter):
-    text = text.replace(/<\/numpourvoi><numpourvoi\s+id=\"\d+\">/gim, ',');
-
-    // Reinject the merged <TEXTE_ARRET> fragments:
-    if (textNextToCatPub === true) {
-      text = text
-        .replace('</CAT_PUB>', '</CAT_PUB><TEXTE_ARRET>' + cleanedTexteArrets.join(' ').trim() + '</TEXTE_ARRET>')
-        .trim();
-    } else if (text.indexOf('</LIEN_WWW>') !== -1) {
-      text = text
-        .replace('</LIEN_WWW>', '</LIEN_WWW><TEXTE_ARRET>' + cleanedTexteArrets.join(' ').trim() + '</TEXTE_ARRET>')
-        .trim();
-    } else {
-      throw new Error(
-        'jurinetLib.cleanText: End of <CAT_PUB> or <LIEN_WWW> tag not found, the document could be malformed or corrupted.',
-      );
-    }
-
-    return text;
+    return replaceTextArrets(text, cleanedTexteArrets);
   },
 };
 
@@ -134,5 +101,51 @@ function cleanTexteArret(texteArret: string) {
       .replace(/&amp;#/g, '&#')
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;');
+  }
+}
+
+function replaceTextArrets(decisionText: string, texteArrets: string[]) {
+  const whereToInjectTexteArrets = computeWhereToInjectTexteArrets(decisionText);
+
+  let newDecisionText = decisionText;
+
+  newDecisionText = removeTextArrets(newDecisionText);
+  newDecisionText = cleanXmlSpecialCharacter(newDecisionText);
+  newDecisionText = serializeNumPourvoi(newDecisionText);
+
+  return newDecisionText
+    .replace(
+      whereToInjectTexteArrets,
+      `${whereToInjectTexteArrets}<TEXTE_ARRET>` + texteArrets.join(' ').trim() + '</TEXTE_ARRET>',
+    )
+    .trim();
+
+  function computeWhereToInjectTexteArrets(str: string) {
+    if (str.indexOf('</CAT_PUB><TEXTE_ARRET>') !== -1) {
+      return '</CAT_PUB>';
+    } else if (str.indexOf('</LIEN_WWW>') !== -1) {
+      return '</LIEN_WWW>';
+    } else {
+      throw new Error(
+        'jurinetLib.cleanText: End of <CAT_PUB> or <LIEN_WWW> tag not found, the document could be malformed or corrupted.',
+      );
+    }
+  }
+
+  function removeTextArrets(str: string) {
+    return str.replace(/<texte_arret>[\s\S]*<\/texte_arret>/gim, '');
+  }
+
+  function cleanXmlSpecialCharacter(str: string) {
+    return str
+      .replace(/&/g, '&amp;')
+      .replace(/&amp;amp;/g, '&amp;')
+      .replace(/&amp;#/g, '&#')
+      .replace(/\s<\s/g, ' &lt; ')
+      .replace(/\s>\s/g, ' &gt; ');
+  }
+
+  function serializeNumPourvoi(str: string) {
+    return str.replace(/<\/numpourvoi><numpourvoi\s+id=\"\d+\">/gim, ',');
   }
 }

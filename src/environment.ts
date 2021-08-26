66aafe27988a58fa2e6739cb12035648d91cda15
BenoitSerrano <benoit.serrano10@gmail.com>
2021-08-26 14:59:18 +0200

    change port for mongo db

--- src/environment.ts
+++ src/environment.ts
@@ -2,6 +2,6 @@ export { environment };
 
 const environment = {
   port: 54321,
-  SDER_DB_URL: 'mongodb://127.0.0.1:27017',
+  SDER_DB_URL: 'mongodb://172.16.118.100:27017',
   SDER_DB_NAME: 'SDER',
 };

8a39ed5228431319c5c9596c2082295ebaeefa64
BenoitSerrano <benoit.serrano10@gmail.com>
2021-10-14 12:50:21 +0200

    handle categoriesToOmit via occultationBlock

---
+++ src/lib/jurinetUtils/convertOccultationBlockToCategoriesToOmit.ts
@@ -0,0 +1,21 @@
+export { convertOccultationBlockInCategoriesToOmit };
+
+function convertOccultationBlockInCategoriesToOmit(occultationBlock: number | undefined) {
+  let categoriesToOmit = ['professionnelMagistratGreffier'];
+  if (occultationBlock !== undefined && occultationBlock >= 1 && occultationBlock <= 4) {
+    switch (occultationBlock) {
+      case 2:
+        categoriesToOmit.push('dateNaissance', 'dateMariage', 'dateDeces');
+        break;
+      case 3:
+        categoriesToOmit.push('personneMorale', 'numeroSiretSiren');
+        break;
+      case 4:
+        categoriesToOmit.push('dateNaissance', 'dateMariage', 'dateDeces', 'personneMorale', 'numeroSiretSiren');
+        break;
+    }
+  } else {
+    categoriesToOmit.push('personneMorale', 'numeroSiretSiren');
+  }
+  return categoriesToOmit;
+}

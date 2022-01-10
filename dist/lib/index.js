a8b40b80f810eb0748e7c5494de974d65ef5df7f
David Libeau <David.Libeau@justice.fr>
2022-01-10 10:58:07 +0100

    Fix lib export

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.jurinetUtils = exports.juricaLib = exports.jurinetLib = void 0;
+exports.juricaUtils = exports.jurinetUtils = exports.juricaLib = exports.jurinetLib = void 0;
 var jurinet_1 = require("./jurinet");
 Object.defineProperty(exports, "jurinetLib", { enumerable: true, get: function () { return jurinet_1.jurinetLib; } });
 var jurica_1 = require("./jurica");
 Object.defineProperty(exports, "juricaLib", { enumerable: true, get: function () { return jurica_1.juricaLib; } });
 var jurinetUtils_1 = require("./jurinetUtils");
 Object.defineProperty(exports, "jurinetUtils", { enumerable: true, get: function () { return jurinetUtils_1.jurinetUtils; } });
+var juricaUtils_1 = require("./juricaUtils");
+Object.defineProperty(exports, "juricaUtils", { enumerable: true, get: function () { return juricaUtils_1.juricaUtils; } });
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import { decisionModule, decisionType } from './modules';
 export { decisionModule };
 
 export type { decisionType };
+
+export * from './lib';

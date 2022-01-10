b429b8510ee6e610bfa7099122c94d16b5805234
David Libeau <DavidLibeau@users.noreply.github.com>
2022-01-10 10:52:32 +0100

    Fix jurica utils (#102)
    * Fix juricaUtils
    
    * Fix utils

--- dist/lib/juricaUtils/index.d.ts
+++ dist/lib/juricaUtils/index.d.ts
@@ -1,12 +1,10 @@
 import { cleanHTML } from './cleanHTML';
-import { getJuricaDuplicate } from './getJuricaDuplicate';
 import { normalize } from './normalize';
 import { removeMultipleSpaces } from './removeMultipleSpaces';
 import { replaceErroneousChars } from './replaceErroneousChars';
-export { jurinetUtils };
-declare const jurinetUtils: {
+export { juricaUtils };
+declare const juricaUtils: {
     cleanHTML: typeof cleanHTML;
-    getJuricaDuplicate: typeof getJuricaDuplicate;
     normalize: typeof normalize;
     removeMultipleSpaces: typeof removeMultipleSpaces;
     replaceErroneousChars: typeof replaceErroneousChars;
--- dist/lib/juricaUtils/index.js
+++ dist/lib/juricaUtils/index.js
@@ -1,16 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.jurinetUtils = void 0;
+exports.juricaUtils = void 0;
 var cleanHTML_1 = require("./cleanHTML");
-var getJuricaDuplicate_1 = require("./getJuricaDuplicate");
 var normalize_1 = require("./normalize");
 var removeMultipleSpaces_1 = require("./removeMultipleSpaces");
 var replaceErroneousChars_1 = require("./replaceErroneousChars");
-var jurinetUtils = {
+var juricaUtils = {
     cleanHTML: cleanHTML_1.cleanHTML,
-    getJuricaDuplicate: getJuricaDuplicate_1.getJuricaDuplicate,
     normalize: normalize_1.normalize,
     removeMultipleSpaces: removeMultipleSpaces_1.removeMultipleSpaces,
     replaceErroneousChars: replaceErroneousChars_1.replaceErroneousChars,
 };
-exports.jurinetUtils = jurinetUtils;
+exports.juricaUtils = juricaUtils;

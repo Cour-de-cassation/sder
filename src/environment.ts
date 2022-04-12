d5e510d2c0d335498fc4441c02a306a5da4055fc
antoinefa <fabien.antoine@m4x.org>
2022-04-12 20:24:14 +0200

    remove ugly file hardcoded env

--- src/environment.ts
+++ src/environment.ts
@@ -1,26 +1,8 @@
 export { getEnvironment };
 
-const runModes = ['PREPROD', 'PROD'] as const;
-type runModeType = typeof runModes[number];
-
 function getEnvironment() {
-  const RUN_MODE = process.env.RUN_MODE as 'PREPROD' | 'PROD' | undefined;
-  if (RUN_MODE && runModes.includes(RUN_MODE)) {
-    return environment[RUN_MODE];
-  } else {
-    throw new Error(`Found no environment for RUN_MODE: ${RUN_MODE}`);
+  return {
+    SDER_DB_URL: process.env.SDER_DB_URL || 'mongodb://dbdser:27017',
+    SDER_DB_NAME: process.env.SDER_DB_NAME || 'SDER',
   }
 }
-
-const environment: Record<runModeType, { port: number; SDER_DB_URL: string; SDER_DB_NAME: string }> = {
-  PREPROD: {
-    port: 54321,
-    SDER_DB_URL: 'mongodb://127.0.0.1:27017',
-    SDER_DB_NAME: 'SDER',
-  },
-  PROD: {
-    port: 54321,
-    SDER_DB_URL: 'mongodb://172.16.118.100:27017',
-    SDER_DB_NAME: 'SDER',
-  },
-};

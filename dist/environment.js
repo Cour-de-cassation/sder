94b7639a60e9c964f4975b09e076194d900cb511
David Libeau <DavidLibeau@users.noreply.github.com>
2022-04-19 17:13:03 +0200

    Typo dbsder (#118)

--- dist/environment.js
+++ dist/environment.js
@@ -4,7 +4,7 @@ exports.getEnvironment = void 0;
 function getEnvironment() {
     return {
         port: 54321,
-        SDER_DB_URL: process.env.SDER_DB_URL || 'mongodb://dbdser:27017',
+        SDER_DB_URL: process.env.SDER_DB_URL || 'mongodb://dbsder:27017',
         SDER_DB_NAME: process.env.SDER_DB_NAME || 'SDER',
     };
 }
